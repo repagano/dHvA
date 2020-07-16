e8cef4f52e79726d7fc1de442558987036329354
iveydavis <iveyedavis2016@gmail.com>
2020-07-15 22:23:13 -0500

    restructuring classes... again..

--- CalculationClass.py
+++ CalculationClass.py
@@ -4,23 +4,9 @@ from scipy import interpolate,signal, optimize
 #import matplotlib.pyplot as plt
 import pandas
 #import csv
-#import os
+import os
 
 #%%
-class ClassdHvALoad():
-    FFmin = 1.5
-    chiup = []
-    FFup = []
-
-class ClassFFTCalc():
-    FFrange = [10,50]
-    ffInv = []
-    chi = []
-    ffInvSpline = []
-    chiSpline = []
-    frequencies= []
-    FFT = []
-    phase = []
     
 class ClassMassCalc():
     m = []
@@ -30,17 +16,22 @@ class ClassMassCalc():
     tempVec = []
     maxFrequency = []
     
-
-class dataProcess(): 
-    inputFrequencyRange = []
-    fn = ''
-    inputTemperature = []
-    dHvALoadObj = ClassdHvALoad()
-    FFTCalcObj = ClassFFTCalc()
-    #MassCalcObj = ClassMassCalc()
+#class dataProcess(): 
+#    inputFrequencyRange = []
+#    fn = ''
+#    inputTemperature = []
+#    dHvALoadObj = ClassdHvALoad()
+#    FFTCalcObj = ClassFFTCalc()
+#    MassCalcObj = ClassMassCalc()
     
+
+class ClassdHvALoad():
+    fn = ''
+    FFmin = 1.5
+    chiup = []
+    FFup = []
     def func_dHvALoad(self):
-        FFmin = self.dHvALoadObj.FFmin
+        FFmin = self.FFmin
         data = pandas.read_csv(self.fn, delimiter = '\t')
         Bdot = np.array(data.Bdot) #dH/dt
         Mdot = np.array(data.Mag1) #dM/dt (Mag1 in ron's code)
@@ -56,22 +47,35 @@ class dataProcess():
             if FFup[ii] != 0:
                 chiup.append(chi[ii])
                 FFup_return.append(FFup[ii])
-        self.dHvALoadObj.chiup = np.array(chiup)
-        self.dHvALoadObj.FFup = np.array(FFup_return)
-        #print('Finished loading dHvA data\n')
-        
+        self.chiup = np.array(chiup)
+        self.FFup = np.array(FFup_return)
+
+class ClassFFTCalc(ClassdHvALoad):
+    FFrange = [10,50]
+    inputFrequencyRange = [400,4000]
+    inputTemperature = float()
+    ffInv = []
+    chi = []
+    ffInvSpline = []
+    chiSpline = []
+    frequencies= []
+    FFT = []
+    phase = []
+    peakFreqs = []
+    peakAmps = []
     def func_FFTCalc(self):
-        if self.dHvALoadObj.FFup != [] and self.dHvALoadObj.chiup != []:
-            FFupi = self.dHvALoadObj.FFup
-            chiupi = self.dHvALoadObj.chiup
-        else:
+        if self.FFup == [] and self.chiup == []:
             print('Loading dHvA data first\n')
-            self.func_dHvALoad(self)
-            FFupi = self.dHvALoadObj.FFup
-            chiupi = self.dHvALoadObj.chiup
+            try:
+                self.func_dHvALoad()
+            except:
+                print("something's wrooooong")
+            
             print('Done loading dHvA data')
-        FFupi[FFupi < self.FFTCalcObj.FFrange[0]] = 0
-        FFupi[FFupi > self.FFTCalcObj.FFrange[1]] = 0
+        FFupi = np.array(self.FFup)
+        chiupi = np.array(self.chiup)
+        FFupi[FFupi < self.FFrange[0]] = 0
+        FFupi[FFupi > self.FFrange[1]] = 0
         nonzeroIndices = np.where(FFupi != 0)[0]
         FFup = FFupi[nonzeroIndices]
         chiup = chiupi[nonzeroIndices]
@@ -118,117 +122,25 @@ class dataProcess():
         fI = np.where((f> self.inputFrequencyRange[0]) & (f < self.inputFrequencyRange[1]))
         f = f[fI]
         
-        self.FFTCalcObj.ffInv = 1/FF
-        self.FFTCalcObj.chi = chi
-        self.FFTCalcObj.chiSpline = chispl
-        self.FFTCalcObj.ffInvSpline = FFinvspl
-        self.FFTCalcObj.frequencies = f
-        self.FFTCalcObj.FFT = FFTval[fI]
-        self.FFTCalcObj.phase = FFTphase[fI]
+        self.ffInv = 1/FF
+        self.chi = chi
+        self.chiSpline = chispl
+        self.ffInvSpline = FFinvspl
+        self.frequencies = f
+        self.FFT = FFTval[fI]
+        self.phase = FFTphase[fI]
+        #print('Finished calculating the FFT\n')
         
-        print('Finished calculating the FFT\n')
-        
-#    def func_FindPeaks(self,minFreq = 150): 
-#        FFT = self.FFTCalcObj.FFT
-#        f = self.FFTCalcObj.frequencies
-#        rms = np.std(FFT)
-#        peakLoc = signal.find_peaks(FFT,height = rms)[0]
-#        peakVal = FFT[peakLoc]
-#        peakFreq = f[peakLoc]
-#        peakValReduced = []
-#        peakFreqReduced = []
-#        
-#        for ii in range(len(peakVal)):
-#            nearbyPeakLocs = np.where(abs(peakFreq[ii] - peakFreq) < 150)
-#            nearbyPeakVals = peakVal[nearbyPeakLocs]
-#            nearbyPeakFreqs = peakFreq[nearbyPeakLocs]
-#            reduced_peak_loc = np.where(peakVal == max(nearbyPeakVals))[0]
-#            if len(peakFreqReduced) != 0 and min(nearbyPeakFreqs) > minFreq:
-#                if max(nearbyPeakVals) != peakValReduced[-1]:
-#                    peakValReduced.append(max(nearbyPeakVals))
-#                    peakFreqReduced.append(peakFreq[reduced_peak_loc][0])
-#            elif len(peakFreqReduced) == 0 and min(nearbyPeakFreqs) > minFreq:
-#                    peakValReduced.append(max(nearbyPeakVals))
-#                    peakFreqReduced.append(peakFreq[reduced_peak_loc][0])
-#                    
-#        return [peakFreq,peakVal]
-       
-#    def func_MassCalc(self):
-#        
-#        if self.FFTCalcObj.frequencies == []:
-#            print('Calculating FFT first\n')
-#            self.FFTCalcObj(self)
-#        [peakFreqs,peakAmps] = self.func_FindPeaks()
-#        f = self.FFTCalcObj.frequencies
-#        FFTvs = self.FFTCalcObj.FFT
-#        tempVec = []
-#        if len(self.MassCalcObj.tempVec) == 0:
-#            print("A vector of temperatures is neeeded for fitting.\n")
-#            print("Assign a vector of temperatures to MassCalcObject.tempVec.")
-#            exit()
-#        elif len(self.MassCalcObj.tempVec) != 0:
-#            tempVec = np.array(self.MassCalcObj.tempVec)
-#            
-#        
-#        A = max(FFTvs)
-#        maxI = FFTvs.argmax()
-#        temp = self.inputTemperature
-#        Brange = self.FFTCalcObj.FFrange
-#        MF = f[maxI]
-#        maxFreq = np.mean(MF)
-#        phaseV = self.FFTCalcObj.phase
-#        phase = phaseV[maxI]
-#        AoT = A/temp
-#        
-#        Bm= 1/(0.5*(1/Brange[0]) + 1/Brange[-1])
-#        fitresult = optimize.least_squares(lambda coeffs:coeffs[0]/np.sinh(coeffs[1]*tempVec) )
-#            
-#        return print('MassCalc')
-        
-class MassCalc():
-    fnVec= []
-    tempVec = []
-    freqRangeVec = []
-    massCalcObj = ClassMassCalc();
-    dataProcObjVec = []
-#    dHvALoadObj = dataProcess.dHvALoadObj();
-#    FFTCalcObj = dataProcess.FFTCalcObj();
-    _dataProcVecNonzero = False
-    
-    def _checkProcVec(self):
-        if len(self.dataProcObjVec) == 0:
-            self._dataProcVecNonzero = False;
-        else:
-            self._dataProcVecNonzero = True
-        return self._dataProcVecNonzero 
-        
-    
-    def func_dataPrep(self):
-        dataProcBool = self._checkProcVec();
-        if dataProcBool == False:
-            if len(self.fnVec) != len(self.tempVec) or len(self.fnVec) != len(self.freqRangeVec) :
-                print("fnVec and tempVec must be the same length")
-                exit();
-            for ii in range(len(self.fnVec)):
-                dataProcObj = dataProcess();
-                dataProcObj.fn = self.fnVec[ii]
-                dataProcObj.inputTemperature = self.tempVec[ii]
-                dataProcObj.inputFrequencyRange = self.freqRangeVec[ii]
-                dataProcObj.func_dHvALoad()
-                dataProcObj.func_FFTCalc()
-                self.dataProcObjVec.append(dataProcObj)
-        
-    def _findPeaks(self,index):
-        FFTObj = self.dataProcObjVec[index].FFTCalcObj;
-        FFT = FFTObj.FFT
-        f = FFTObj.frequencies
+    def func_findPeaks(self):
+        FFT = self.FFT
+        f = self.frequencies
         rms = np.std(FFT)
         peakLoc = signal.find_peaks(FFT,height = rms)[0]
         peakVal = FFT[peakLoc]
         peakFreq = f[peakLoc]
         peakValReduced = []
         peakFreqReduced = []
-        minFreq = self.dataProcObjVec[index].inputFrequencyRange[0];
+        minFreq = self.inputFrequencyRange[0];
         for ii in range(len(peakVal)):
             nearbyPeakLocs = np.where(abs(peakFreq[ii] - peakFreq) < 50)
             nearbyPeakVals = peakVal[nearbyPeakLocs]
@@ -242,27 +154,70 @@ class MassCalc():
                     peakValReduced.append(max(nearbyPeakVals))
                     peakFreqReduced.append(peakFreq[reduced_peak_loc][0])
                     
-        return [peakFreq,peakVal]
+        self.peakFreqs = peakFreqReduced
+        self.peakAmps = peakValReduced
+
+
+class MassCalc():
+    fnVec= []
+    tempVec = []
+    freqRangeVec = []
+    massCalcObj = ClassMassCalc();
+    dataProcObjVec = []
+    FFTObj = ClassFFTCalc();
+#    dHvALoadObj = dataProcess.dHvALoadObj();
+#    FFTCalcObj = dataProcess.FFTCalcObj();
+    _dataProcVecNonzero = False
     
+    def _checkProcVec(self):
+        if len(self.dataProcObjVec) != len(self.tempVec):
+            self._dataProcVecNonzero = False;
+        else:
+            self._dataProcVecNonzero = True
+        return self._dataProcVecNonzero 
+        
+    
+    def func_dataPrep(self):
+        dataProcBool = self._checkProcVec();
+        if dataProcBool == False:
+            if len(self.fnVec) != len(self.tempVec) or len(self.fnVec) != len(self.freqRangeVec) :
+                print("fnVec and tempVec must be the same length")
+                exit();
+            for ii in range(len(self.fnVec)):
+                dataProcObj = ClassFFTCalc();
+                dataProcObj.fn = self.fnVec[ii];#print(dataProcObj.fn)
+                dataProcObj.inputTemperature = self.tempVec[ii]
+                dataProcObj.inputFrequencyRange = self.freqRangeVec[ii]
+                dataProcObj.func_dHvALoad()
+                dataProcObj.func_FFTCalc()
+                dataProcObj.func_findPeaks()
+                self.dataProcObjVec.append(dataProcObj)
+#            for ii in self.dataProcObjVec:
+#                print(len(ii.peakFreqs))
+            #print('\n\n')
+            #print(self.dataProcObjVec[0].FFTCalcObj.peakFreqs,',',self.dataProcObjVec[3].FFTCalcObj.peakFreqs)
+       
     def _ComparePeaks(self):
         peakFreqs = []
         peakAmps = []
         significantPeaks = np.empty((0,0))
-        for ii in range(len(self.dataProcObjVec)):
-            [freqs,amps] = self._findPeaks(self,ii)
-            peakFreqs.append(freqs)
+        for ii in self.dataProcObjVec:
+            freqs = ii.peakFreqs
+            amps = ii.peakAmps
+            peakFreqs.append(freqs); 
             peakAmps.append(amps)
         
-        for ii in range(len(self.dataProcObjVec)):
+        for ii in range(len(self.tempVec)):
             currentFreqs = np.array(peakFreqs[ii])
-            for jj in range(len(self.dataProcObjVec)):
+            for jj in range(len(self.tempVec)):
                 testFreq = np.array(peakFreqs[ii])
                 if ii != jj:
                     for kk in range(len(currentFreqs)):
-                        simFreqLoc = np.where(abs(currentFreqs[kk]-testFreq) <= 10)
+                        simFreqLoc = np.where(abs(currentFreqs[kk]-testFreq) <= 10)[0]
                         if simFreqLoc.size != 0:
                             significantPeaks = np.append(significantPeaks, testFreq[simFreqLoc])
         significantPeaks = np.unique(significantPeaks)
+        
         
         refinedFrequencies = np.empty((0,0))
         for ii in range(len(significantPeaks)):
@@ -270,6 +225,7 @@ class MassCalc():
             simFreqLocs = np.where(abs(currentFreq - significantPeaks)<9)[0]
             if simFreqLocs.size > 5:
                 refinedFrequencies = np.append(refinedFrequencies,significantPeaks[simFreqLocs])
+                #print(significantPeaks[simFreqLocs],'\n')
         
         refinedFrequencies = np.unique(refinedFrequencies)
         refinedFrequencies = list(refinedFrequencies)
@@ -292,61 +248,65 @@ class MassCalc():
                     refinedFrequencies.remove(badFreq)
                 ind = 0
         return np.unique(refinedFrequencies)
-#        finalFrequencies = []
-#        for ii in range(len(refinedFrequencies)):
-#            currentFreq = refinedFrequencies[ii]
-#            if ii != 0:
-#                if currentFreq - refinedFrequencies[ii-1] < 20:
-#                    finalFrequencies.append((currentFreq + refinedFrequencies[ii-1])/2)
-#                else:
-#                    finalFrequencies.append(currentFreq)
-#            if ii == 0:
-#                finalFrequencies.append(currentFreq)
-#        return np.unique(np.array(finalFrequencies));
+
         
-    def _findCorresponingAmp(self,ind):
+    def _findCorresponingAmp(self,ind,commonPeakFreqs):
         commonPeakFreqs = self._ComparePeaks()
-        FFTObj = self.dataProcObjVec[ind].FFTCalcObj
+        FFTObj = self.dataProcObjVec[ind]
         FFT = np.array(FFTObj.FFT)
         f = np.array(FFTObj.frequencies)
         peakAmp = []
         rmsAmp = []
         peakFreq = []
-        #want to find where the max value occurs and then do a window around IT
         for freqPeak in commonPeakFreqs:
             #oom = np.floor(np.log10(freqPeak))
             freqWindow = 0.02*freqPeak
             fWindowMin = freqPeak - freqWindow
             fWindowMax = freqPeak + freqWindow
             searchLoc = np.where((f > fWindowMin) & (f < fWindowMax))[0]
+            
             ampVec = FFT[searchLoc]
             maxAmp = ampVec.max()
-            maxInd = np.where(FFT = maxAmp)
+            maxInd = np.where(FFT == maxAmp)
             maxFreq = f[maxInd]
-            rms = np.std(ampVec)
+            
+            newWindow = [maxFreq - maxFreq*0.075, maxFreq + maxFreq*0.075]
+            newWindowInd = np.where( (f > newWindow[0]) & (f < newWindow[1]))
+            newWindowAmp = FFT[newWindowInd]
+            rms = np.std(newWindowAmp)
+            
             peakAmp.append(maxAmp)
             peakFreq.append(maxFreq)
             rmsAmp.append(rms)
         return peakFreq, peakAmp, rmsAmp
-                
-            
-        
-            
-    
 
         
     def func_MassCalc(self):
-        if self._checkProcVec == False:
+        if self._checkProcVec() == False:
             self.func_dataPrep()
-        for dataProcObj in self.dataProcObjVec:
-            FFTObj = dataProcObj.FFTCalcObj;
+        commonPeakFreqs = self._ComparePeaks()
+        tempVec = self.tempVec
+        tempLength = len(tempVec)
+        freqLength = len(commonPeakFreqs)
+        amplitudeMat = np.empty((tempLength,freqLength)) #horizontal axis: frequency, vertical axis: temperature
+        frequencyMat = np.empty((tempLength,freqLength))
+        rmsMat = np.empty((tempLength,freqLength))
+        
+        for ii in range(len(tempVec)):
+            [peakFreq,peakAmp,rmsAmp] = self._findCorresponingAmp(ii,commonPeakFreqs)
+            amplitudeMat[ii][:] = peakAmp
+            frequencyMat[ii][:] = peakFreq
+            rmsMat[ii][:] = rmsAmp
+            
+        
+            
+            
+        
             
         
         
                 
-    
-            
- 
+#%%    
 folder_path = r'C:\Users\iveli\OneDrive\Documents\Ron\031919'
 file_paths = []
 
@@ -356,17 +316,32 @@ for file in os.listdir(folder_path):
         
 temps = [1.52, 0.62, 5.71, 3.96, 8, 10, 12, 14, 18, 3.27, 2.24, 0.96];    
 
-#testObj = dataProcess()
-#testObj.fn = file_paths[0]
-#testObj.inputTemperature = temps[0]
-#testObj.inputFrequencyRange = [10,8000]
-#
-#testObj.func_dHvALoad()
-#testObj.func_FFTCalc()
+testObj = dataProcess()
+testObj.fn = file_paths[0]
+testObj.inputTemperature = temps[0]
+testObj.inputFrequencyRange = [10,8000]
+
+testObj.func_dHvALoad()
+testObj.func_FFTCalc()
+testObj.func_findPeaks()
+
+#for ii in range(len(temps)):
+#    testObj = dataProcess()
+#    testObj.fn = file_paths[ii]
+#    testObj.inputTemperature = temps[ii]
+#    testObj.inputFrequencyRange = [450,4000]
+#    
+#    testObj.func_dHvALoad()
+#    testObj.func_FFTCalc()
+#    testObj.func_findPeaks()
+#    plt.plot(testObj.FFTCalcObj.frequencies,testObj.FFTCalcObj.FFT)
+#    plt.scatter(testObj.FFTCalcObj.peakFreqs,testObj.FFTCalcObj.peakAmps)
+
 
 masstestObj = MassCalc()
 masstestObj.fnVec = file_paths
 masstestObj.tempVec = temps
-frang= [400,4000]
+frang= [450,4000]
 masstestObj.freqRangeVec = [frang]*len(file_paths)
 masstestObj.func_dataPrep()
+masstestObj.func_MassCalc()
